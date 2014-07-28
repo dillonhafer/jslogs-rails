aef03d2deb1436a10f5e5855e586d1ca3b2a1e08
Dillon Hafer <dh@dillonhafer.com>
2014-07-28 16:09:15 -0400

    Adds rake task for finding jslog uses

--- Rakefile
+++ Rakefile
@@ -1,1 +1,9 @@
 require "bundler/gem_tasks"
+
+namespace :jslogs do
+
+  desc "Find where you're using jslogs."
+  task :find do
+    %x{grep -R -n 'jslogs(' app/*}
+  end
+end
--- vendor/assets/javascripts/jslogs.js
+++ vendor/assets/javascripts/jslogs.js
@@ -1,11 +1,10 @@
 function jslogs(message) {
   var new_text = "%c";
   var styles   = "";
-  
+
   new_text += message;
   styles   += "color: #69F;";
   styles   += "font-family: Helvetica;";
   styles   += "font-size: 18px;";
-  
   console.log(new_text, styles);
 }
